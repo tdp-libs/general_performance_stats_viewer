acfb86d46d2f2934cd4756ff6256c1cfc80fbebb
Tom Paynter <tompaynter@tdpe.co.uk>
2019-11-27 17:25:44 +0000

    Improvements to graphing.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,12 +4,15 @@
 
 #include "tp_maps/layers/PointsLayer.h"
 #include "tp_maps/textures/DefaultSpritesTexture.h"
+#include "tp_maps/layers/LinesLayer.h"
 
 #include <QBoxLayout>
 #include <QSplitter>
 #include <QListWidget>
 #include <QPushButton>
 #include <QFileDialog>
+#include <QMenu>
+#include <QCursor>
 
 #include <fstream>
 #include <iostream>
@@ -36,10 +39,12 @@ struct MainWindow::Private
   MainWindow* q;
 
   QListWidget* listWidget{nullptr};
+  QMenu* listWidgetMenu{nullptr};
 
   tp_qt_maps_widget::MapWidget* mapWidget{nullptr};
 
-  std::vector<tp_maps::Layer*> layers;
+  std::vector<tp_maps::Layer*> pointLayers;
+  std::vector<tp_maps::Layer*> lineLayers;
 
   //################################################################################################
   Private(MainWindow* q_):
@@ -89,8 +94,12 @@ struct MainWindow::Private
         trace->maxValue = value;
     }
 
-    tpDeleteAll(layers);
-    layers.clear();
+    tpDeleteAll(pointLayers);
+    pointLayers.clear();
+
+    tpDeleteAll(lineLayers);
+    lineLayers.clear();
+
     listWidget->clear();
 
     size_t t=0;
@@ -107,22 +116,36 @@ struct MainWindow::Private
 
       //Style the points and prepare them for rendering.
       std::vector<tp_maps::PointSpriteShader::PointSprite> points;
+      tp_maps::Lines line;
+      line.mode = GL_LINE_STRIP;
+      line.color = colorF;
       points.resize(trace->points.size());
+      line.lines.resize(trace->points.size());
       for(size_t p=0; p<trace->points.size(); p++)
       {
         const auto& src = trace->points.at(p);
         auto& dst = points.at(p);
         dst.position = glm::vec3(float(src.first) / float(i) * 8.0f, float(src.second) / float(trace->maxValue), 0.0f);
         dst.color = colorF;
-        dst.radius = 1.5f;
+        dst.radius = 2.5f;
+        line.lines.at(p) = dst.position;
       }
 
-      auto spriteTexture = new tp_maps::SpriteTexture();
-      spriteTexture->setTexture(new tp_maps::DefaultSpritesTexture(mapWidget->map()));
-      auto layer = new tp_maps::PointsLayer(spriteTexture);
-      layer->setPoints(points);
-      mapWidget->map()->addLayer(layer);
-      layers.push_back(layer);
+      {
+        auto spriteTexture = new tp_maps::SpriteTexture();
+        spriteTexture->setTexture(new tp_maps::DefaultSpritesTexture(mapWidget->map()));
+        auto layer = new tp_maps::PointsLayer(spriteTexture);
+        layer->setPoints(points);
+        mapWidget->map()->addLayer(layer);
+        pointLayers.push_back(layer);
+      }
+
+      {
+        auto layer = new tp_maps::LinesLayer();
+        layer->setLines({line});
+        mapWidget->map()->addLayer(layer);
+        lineLayers.push_back(layer);
+      }
 
       t++;
     }
@@ -134,11 +157,45 @@ struct MainWindow::Private
   void itemChanged(QListWidgetItem* item)
   {
     auto row = size_t(listWidget->row(item));
-    if(row<layers.size())
+    if(row<pointLayers.size())
     {
-      layers.at(row)->setVisible(item->checkState() == Qt::Checked);
+      pointLayers.at(row)->setVisible(item->checkState() == Qt::Checked);
       mapWidget->map()->update();
     }
+
+    if(row<lineLayers.size())
+    {
+      lineLayers.at(row)->setVisible(item->checkState() == Qt::Checked);
+      mapWidget->map()->update();
+    }
+  }
+
+  //################################################################################################
+  void showSelected()
+  {
+    for(auto i : listWidget->selectedItems())
+      i->setCheckState(Qt::Checked);
+  }
+
+  //################################################################################################
+  void hideSelected()
+  {
+    for(auto i : listWidget->selectedItems())
+      i->setCheckState(Qt::Unchecked);
+  }
+
+  //################################################################################################
+  void showAll()
+  {
+    for(int i=0; i<listWidget->count(); i++)
+      listWidget->item(i)->setCheckState(Qt::Checked);
+  }
+
+  //################################################################################################
+  void hideAll()
+  {
+    for(int i=0; i<listWidget->count(); i++)
+      listWidget->item(i)->setCheckState(Qt::Unchecked);
   }
 };
 
@@ -162,6 +219,14 @@ MainWindow::MainWindow():
   connect(d->listWidget, &QListWidget::itemChanged, [&](QListWidgetItem* item){d->itemChanged(item);});
   d->listWidget->setSelectionMode(QAbstractItemView::ExtendedSelection);
   d->listWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+  d->listWidget->setContextMenuPolicy(Qt::CustomContextMenu);
+  connect(d->listWidget, &QWidget::customContextMenuRequested, [&](const QPoint& pos){d->listWidgetMenu->exec(d->listWidget->mapToGlobal(pos));});
+
+  d->listWidgetMenu = new QMenu(d->listWidget);
+  connect(d->listWidgetMenu->addAction("Show selected"), &QAction::triggered, [&]{d->showSelected();});
+  connect(d->listWidgetMenu->addAction("Hide selected"), &QAction::triggered, [&]{d->hideSelected();});
+  connect(d->listWidgetMenu->addAction("Show all"),      &QAction::triggered, [&]{d->showAll();     });
+  connect(d->listWidgetMenu->addAction("Hide all"),      &QAction::triggered, [&]{d->hideAll();     });
 
   auto loadButton = new QPushButton("Load");
   leftLayout->addWidget(loadButton);
